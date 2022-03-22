01db4e3159c205e678255404d237e23cd4040034
TaeyongKimOST <tkim@ostusa.com>
2022-03-22 19:48:03 -0400

    basic stuff started

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Assignment 17 - Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <?php echo date('l, F jS, Y'); ?>
+</body>
+
+</html>
